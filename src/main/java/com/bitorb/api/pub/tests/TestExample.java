319f3c829a883c27c9ecd72d993c8121a0585d0d
Daria404 <roshinadaria@gmail.com>
2021-01-29 15:45:49 +0300

    websocket is added

--- src/main/java/com/bitorb/api/pub/tests/TestExample.java
+++ src/main/java/com/bitorb/api/pub/tests/TestExample.java
@@ -5,6 +5,7 @@ import net.openhft.chronicle.wire.WireType;
 import net.openhft.chronicle.wire.Wires;
 import net.openhft.chronicle.wire.WriteMarshallable;
 import okhttp3.*;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,28 +17,48 @@ public class TestExample extends WebSocketListener {
     private static final String HOST = "devcomp.bitorb.com";
     private static final String REQUEST_PATH = "/api/v1";
     private static final String contracturl = "https://" + HOST + REQUEST_PATH;
+    private static final String SUBSCRIBE_PATH = "/api/v1/subscribe";
     private static final String APISECRET = "12345"; //TODO
     private static final String APIKEY = "!/f31<rq)6OdPF>KuySkqu3bFTHj+@_$eXjc+;UcxT%j8Y&G_%LlZS!>5SEn40kuA6_DUI((!@VeOsyK/h0P!p-wV7WvO?!7Lxq%vgZ5I!>!o*2T1mF!Y+FnVmp%wXjbu#cSr!9;Z8BIGXzPV.(knuh.PI;+GAxTl1!i-zcSy#l/rJ!.<m3s@aopL/.!k!fGQCky#T<h68W/TOk6oh#RV!l0fxfH3!s6wp%>%eB1fNG(Svxd-X0@'t%0oV/!2-z;#zvvPjPo9SAjCQnm.B+cvJyW'wR*k<AgC'h8HVl;+JPd+#ZwVecf(J#1k_XgHa"; //TODO
     private static final String USER = "testuser";
     private final OkHttpClient client = new OkHttpClient();
     private final OkHttpClient websocketClient = new OkHttpClient.Builder().pingInterval(java.time.Duration.ofSeconds(5)).build();
 
+    @Override
+    public void onOpen(WebSocket webSocket, Response response) {
+        boolean test = webSocket.send("test");
+        System.out.println(test);
+    }
+    @Override
+    public void onMessage(WebSocket webSocket, String text) {
+        System.out.println("Received message:" + text);
+    }
+    @Override
+    public void onClosed(WebSocket webSocket, int code, String reason) {
+        webSocket.close(1000, "bye");
+    }
+
+    @Override
+    public void onFailure(WebSocket webSocket, Throwable t, @Nullable Response response) {
+        t.printStackTrace();
+    }
+
     @Test
-    public void testSymbolInfo() {
+    public void testSymbolInfo() throws InterruptedException {
         String expires = "" + (System.currentTimeMillis() + 60_000);
 
-        final String signature = HashUtils.getSecretHash(APISECRET, APIKEY + expires + REQUEST_PATH + "/symbolInfo");
+        final String signature = HashUtils.getSecretHash(APISECRET, APIKEY + expires + SUBSCRIBE_PATH);
 
         final Request request = new Request.Builder()
-                .url(contracturl + "/symbolInfo")
-                .get()
+                .url("wss://" + HOST + SUBSCRIBE_PATH)
                 .header("api-key", APIKEY)
                 .header("api-expires", expires)
                 .header("api-signature", signature)
                 .build();
 
-        TestExample socketlistener = new TestExample();
-        WebSocket webSocket = websocketClient.newWebSocket(request, socketlistener);
+//        TestExample socketlistener = new TestExample();
+        WebSocket webSocket = websocketClient.newWebSocket(request, this);
+        Thread.sleep(10000);
         boolean subscribed = webSocket.send(
                 "{" +
                         "op: subscribe, " +
@@ -46,17 +67,15 @@ public class TestExample extends WebSocketListener {
                         "symbols: BTC_USD_P0" + // If no specific symbol(s) then will receive all symbols
                         "}" +
                         "}");
+//        System.out.println(subscribed);
 
-        try (Response resp = client.newCall(request).execute()) {
-            assertTrue(resp.toString(), resp.isSuccessful());
-            System.out.println(resp.code());
-            socketlistener.onOpen(webSocket, resp);
-            socketlistener.onMessage(webSocket, "test");
-            System.out.println(webSocket.request());
-        } catch (IOException ex) {
-            fail("Error: " + ex);
-        }
-    }
+//       try (Response resp = client.newCall(request).execute()) {
+//            assertTrue(resp.toString(), resp.isSuccessful());
+//
+//       } catch (IOException ex) {
+//           fail("Error: " + ex);
+//       }
+       }
 
     @Test
     public void userWallet() {
@@ -74,7 +93,6 @@ public class TestExample extends WebSocketListener {
 
         try (Response resp = client.newCall(request).execute()) {
             assertTrue(resp.toString(), resp.isSuccessful());
-            System.out.println(resp.code());
 
         } catch (IOException ex) {
             fail("Error: " + ex);
@@ -106,7 +124,6 @@ public class TestExample extends WebSocketListener {
                 .build();
 
         try (Response resp = client.newCall(request).execute()) {
-            System.out.println(resp.code());
             assertTrue(resp.toString(), resp.isSuccessful());
 //            assertEquals(Side.SELL, createOrder.side());
 //            assertEquals(100.0, createOrder.qty(), 0.000001);
@@ -141,7 +158,6 @@ public class TestExample extends WebSocketListener {
                 .build();
 
         try (Response resp = client.newCall(request).execute()) {
-            System.out.println(resp.code());
             assertTrue(resp.isSuccessful());
 //            assertEquals(clOrdID, unwindOrder.clOrdID());
 //            assertTrue(Double.isNaN(unwindOrder.qty()));
